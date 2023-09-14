92f7aee42045b3c8dc85762077464a979b784b1e
joshbadger <badger.joshua@gmail.com>
2023-09-14 10:02:01 -0600

    ifs, use $HOME instead of ~

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ export HOMEBREW_INSTALL_FROM_API=1
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # TODO: install xcode command line tools
+# or maybe not - mac installs this the first time you run any git command
 
 # set -x  # this prints commands to screen
 
@@ -14,15 +15,15 @@ CASKS=(
 	boom  # system-wide mac EQ
 	brave-browser  # no tracky tracky
 	insomnia  # rest client
-  iterm2
-	karabiner-elements  # for key mapping my HHKB function keys
+	iterm2
+      # karabiner-elements  # for key mapping my HHKB function keys
 	menumeters  # menu bar monitors (cpu, temp, network, etc)
-	pgadmin4
+      # pgadmin4
 	slack
 	spectacle  # window mgmt
 	spotify
 	visual-studio-code
-  postgresql@14
+      # postgresql@14
 )
 
 NOT_CASKS=(
@@ -48,20 +49,37 @@ for i in "${NOT_CASKS[@]}"; do
   brew install "$i" || true
 done
 
-echo "setting up dotfiles and ~/src dirs"
-mkdir ~/src
-git -C ~/src clone https://github.com/joshbadger/dotfiles.git
+echo "setting up dotfiles and $HOME/src dirs"
 
-echo "sym-linking dotfiles"
+if [ -d "$HOME/src" ]; then
+  echo "Directory $HOME/src exists."
+else
+  mkdir $HOME/src
+fi
+
+if [-d "$HOME/src/dotfiles"]; then
+  echo "dotfiles have already been cloned"
+else
+  git -C $HOME/src clone https://github.com/joshbadger/dotfiles.git
+fi
+
+echo "sym-linking dotfiles for:"
+echo "  vim"
+echo "  bash_profile"
+echo "  bash_functions"
+echo "  git completion"
+echo "  prompt"
+echo "  vscode settings + keybindings"
+
 cd
-ln -s ~/src/dotfiles/.vimrc ~
-ln -s ~/src/dotfiles/.bash_profile ~
-ln -s ~/src/dotfiles/.bash_functions ~
-ln -s ~/src/dotfiles/bin ~  # for git completion and prompt
-ln -s ~/src/dotfiles/keybindings.json ~
-ln -s ~/src/dotfiles/settings.json ~
+ln -s $HOME/SRC/dotfiles/.vimrc $HOME
+ln -s $HOME/src/dotfiles/.bash_profile $HOME
+ln -s $HOME/src/dotfiles/.bash_functions $HOME
+ln -s $HOME/src/dotfiles/bin $HOME  # for git completion and prompt
+ln -s $HOME/src/dotfiles/keybindings.json $HOME
+ln -s $HOME/src/dotfiles/settings.json $HOME
 
 # vscode enable press and hold for key repeat
 eval "defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false"
 
-source ~/.bash_profile
+source $HOME/.bash_profile
